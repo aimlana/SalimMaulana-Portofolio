6b598da49485816213e34494f5cbfc44ca7b31fc
Muh. Salim Maulana <salim.yaklu@gmail.com>
2024-11-18 13:07:50 +0800

    refactor: :wrench: Config tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        alumni: ['Alumni Sans', 'sans-serif'],
+        allison: ['Allison', 'cursive'],
+      },
+      fontSize: {
+        '10xl': ['10rem', { lineHeight: '1' }],
+      },
+      colors: {
+        black: '#2C2C2C',
+      },
+    },
   },
   plugins: [],
-}
-
+};
