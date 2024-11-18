28fc38ab2f2753f04c546182129ddb02c5c2bf8d
Muh. Salim Maulana <salim.yaklu@gmail.com>
2024-11-18 13:23:20 +0800

    feat: :sparkles: Add Home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import HeroSection from "../components/HeroSection";
+import InfiniteSlider from "../components/InfiniteSlider";
+
+function Home() {
+  return (
+    <>
+      <Header />
+      <HeroSection />
+      <InfiniteSlider />
+      <Footer />
+    </>
+  );
+}
+
+export default Home;
