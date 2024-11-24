07c19215a3403ae86c2cc1719c6288167d5845d2
Muh. Salim Maulana <salim.yaklu@gmail.com>
2024-11-24 23:40:14 +0800

    style: :lipstick: Remove margin top

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 function Footer() {
   return (
     <>
-      <footer className="bg-black py-1 mt-60">
+      <footer className="bg-black py-1">
         <h1 className="text-[10vw] font-alumni text-center text-white">Ini Footer</h1>
       </footer>
     </>
